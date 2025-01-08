85e8d53ff26b24758f1d3014c70e8117d5e473de
ali94n0 <ali94n0@gmail.com>
2025-01-08 10:36:38 +0330

    project done 12

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ import { RiTelegram2Line, RiWhatsappLine } from "react-icons/ri";
 function Footer(props) {
     return (
         <div className='w-full flex flex-col justify-center items-center bg-secondary-300 py-6 max-w-screen-sm mx-auto gap-y-2'>
-            <div className='flex items-center gap-x-8'>
+            <div className='flex items-center gap-x-6'>
                 <Link href={"https://t.me/vpnvip_support"}><RiTelegram2Line className='w-6 h-6 fill-secondary-600'/></Link>
                 <Link href={"https://wa.me/+15074147481"}><RiWhatsappLine className='w-6 h-6 fill-secondary-600' /></Link>
             </div>
